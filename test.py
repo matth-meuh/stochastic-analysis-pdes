b7cb3765a32c071b4b187aeaf4c1290957412c9b
Matthieu Meunier <matth.meuh@gmail.com>
2024-04-22 16:59:38 +0100

    refactored code for particles

--- test.py
+++ test.py
@@ -2,6 +2,17 @@ import numpy as np
 import heapq
 from scipy.stats import gamma
 
+class Particle:
+    def __init__(self, death_x, birth_t, death_t, wk, ik, mark = 0, birth_x = 0):
+        self.mark = mark
+        self.birth_x = birth_x
+        self.death_x = death_x
+        self.birth_t = birth_t
+        self.death_t = death_t
+        self.wk = wk
+        self.ik = ik
+    
+
 class BranchingProcess:
     def __init__(self, T=1.0, alpha=0.2, c = 0.15, d=5, x0=None, kappa=0.5, theta=2.5, n=96000, M=1000):
         self.T = T
@@ -24,20 +35,15 @@ class BranchingProcess:
         self.time = 0.0
         self.next_splitting_time = []
         split_time = min(np.random.gamma(shape = self.kappa, scale = self.theta), self.T)
+        p0 = Particle(
+            birth_x = self.x0,
+            death_x= self.x0 + np.sqrt(split_time)*self.sigma0*np.random.normal(size=self.d),
+            birth_t = 0, 
+            death_t= split_time,
+            wk= 1.0, mark = 0, ik = np.random.binomial(n = 1, p = 0.5)
+            )
         heapq.heappush(self.next_splitting_time, (split_time, (1,)))
-        
-        # population dict keeps track of data of all particles
-        # key is particle lable, 
-        # value is (mark, birth state X_{T_k-}, death state X_{T_k}, birth time T_k-, death time T_k, value of W_k, value of splitting I_k)
-        self.population = {(1,): (
-            0, 
-            self.x0, 
-            self.x0 + np.sqrt(split_time)*self.sigma0*np.random.normal(size=self.d), 
-            0.0, 
-            split_time,
-            1.0,
-            np.random.binomial(n = 1, p = 0.5)
-            )}
+        self.population = {(1,): p0}
 
     def step(self):
         (tau, label) = heapq.heappop(self.next_splitting_time)
@@ -45,22 +51,42 @@ class BranchingProcess:
         self.time = tau
         if tau >= self.T:
             return
-        i = self.population[label][-1]
+        parent = self.population[label]
+        i = parent.ik
         if i == 1:
             l1,l2 = label + (1,), label + (2,)
-            x = self.population[label][2]
+            x = parent.death_x
             s1,s2 = np.random.gamma(shape = self.kappa, scale = self.theta, size = 2)
-            s1,s2 = min(s1 + tau, self.T), min(s2 + tau,self.T) 
+            death_t1,death_t2 = min(s1 + tau, self.T), min(s2 + tau,self.T) 
             i1,i2 = np.random.binomial(n = 1, p = 0.5, size = 2) 
-            heapq.heappush(self.next_splitting_time, (s1, l1))
-            heapq.heappush(self.next_splitting_time, (s2, l2))
-            dw1 = np.sqrt(s1 - tau) * np.random.normal(size = self.d)
-            self.population[label + (1,)] = (0, x, x + self.sigma0*dw1, tau, s1, 1.0, i1)
+            heapq.heappush(self.next_splitting_time, (death_t1, l1))
+            heapq.heappush(self.next_splitting_time, (death_t2, l2))
+
+            dw1 = np.sqrt(death_t1 - tau) * np.random.normal(size = self.d)
+            child1 = Particle(
+                mark = 0,
+                birth_x = x,
+                death_x = x + self.sigma0*dw1,
+                birth_t= tau,
+                death_t=death_t1,
+                wk=1.0,
+                ik=i1
+            )
+            self.population[l1] = child1
             
-            dw2 = np.sqrt(s2 - tau)*np.random.normal(size = self.d)
-            wbar =  dw2 / ((s2 - tau)*self.sigma0)
+            dw2 = np.sqrt(death_t2 - tau)*np.random.normal(size = self.d)
+            wbar =  dw2 / ((death_t2 - tau)*self.sigma0)
             wk = self.b @ wbar 
-            self.population[label + (2,)] = (1, x, x + self.sigma0*dw2, tau, s2, wk, i2)                    
+            child2 = Particle(
+                mark = 1,
+                birth_x=x,
+                death_x=x + self.sigma0*dw2,
+                birth_t=tau,
+                death_t=death_t2, 
+                wk=wk, 
+                ik=i2
+            )                    
+            self.population[l2] = child2
 
     def cl(self,idx,t,x):
         if idx == 0:
@@ -88,14 +114,14 @@ class BranchingProcess:
         self.sample_process()
         res = 1
         for l in self.population:
-            mark, birth_x, death_x, birth_t, death_t, wk, ik = self.population[l]
-            if death_t >= self.T:
-                g1 = self.g(death_x)
-                g2 = self.g(birth_x) if mark > 0 else 0
-                f = self.fbar(death_t - birth_t)
-                res *= (g1 - g2) * wk / f
+            p = self.population[l]
+            if p.death_t >= self.T:
+                g1 = self.g(p.death_x)
+                g2 = self.g(p.birth_x) if p.mark > 0 else 0
+                f = self.fbar(p.death_t - p.birth_t)
+                res *= (g1 - g2) * p.wk / f
             else:
-                res *= self.cl(ik, death_t, death_x) * wk * 2 / (self.rho(death_t - birth_t))
+                res *= self.cl(p.ik, p.death_t, p.death_x) * p.wk * 2 / (self.rho(p.death_t - p.birth_t))
         return res
 
     def sample_expectation_psi(self):
@@ -117,5 +143,4 @@ class BranchingProcess:
         return np.cos(sum(self.x0)) * np.exp(self.alpha * self.T)
                 
 bp = BranchingProcess()
-
-bp.sample_psi()
+bp.sample_expectation_psi()
